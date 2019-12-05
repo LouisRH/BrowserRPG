001c683875765317628684c77bef410c32ea75d1
LouisRH <louisrh999@gmail.com>
2019-12-05 16:28:29 -0500

    Connected to hosted mongodb database (no longer local)

--- mongoConnection.js
+++ mongoConnection.js
@@ -5,10 +5,11 @@ const mongoConfig = settings.mongoConfig;
 let fullMongoUrl = `${mongoConfig.serverUrl}`;
 let _connection = undefined;
 let _db = undefined;
+let url = process.env.MONGODB_URI;
 
 module.exports = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(fullMongoUrl, { useNewUrlParser: true });
+    _connection = await MongoClient.connect(url, { useNewUrlParser: true });
     _db = await _connection.db(mongoConfig.database);
   }
 
